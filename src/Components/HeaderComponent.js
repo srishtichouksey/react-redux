e220b0689972a5f12e83be25ffa613b3cfcc727e
srishti chouksey <srishtichouksey175@gmail.com>
2018-08-01 09:27:04 +0530

    Creating Header Footer second commit

---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Jumbotron } from 'reactstrap';
+
+export default class Header extends Component {
+	render() {
+		return(
+			<React.Fragment>
+				<Jumbotron>
+					<div className="container">
+						<div className=" row row-header">
+							<div className="col-12 col-sm-6">
+								<h1>Kitchen King</h1>
+							</div>
+						</div>
+					</div>
+				</Jumbotron>
+			</React.Fragment>
+		);
+	}
+}
